c31fdcd3e8548d1e8fa8088e82df24a5bc5c0879
Michael Eyitayo <archangelmichael516@gmail.com>
2020-08-26 19:58:45 +0100

    Row

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as UI from "./lib/new";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <UI.Row justifyEnd alignCenter>
+        <Text>Test</Text>
+        <Text style={{ fontSize: 22 }}>Test2</Text>
+      </UI.Row>
     </View>
   );
 }
@@ -14,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
